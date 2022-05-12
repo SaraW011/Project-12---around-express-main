86614081606c53cba3e609eeb12fdc0f449ac027
SaraW011 <87926715+SaraW011@users.noreply.github.com>
2022-05-12 07:05:25 +0300

    find single user update

--- controllers/users.js
+++ controllers/users.js
@@ -16,14 +16,14 @@ const getUsers = async (req, res) => {
 const getUserById = async (req, res) => {
   try {
     const users = await readJsonFile(usersFilePath);
-    const id = users.find((user) => user.id === req.params._id);
+    const id = users.find((user) => user._id === req.params._id);
     if (!id) {
       res.status(404).send({ message: "User ID not found" });
     } else {
-      res.status(200).send(users);
+      res.status(200).send(id);
     }
   } catch (err) {
-    res.status(500).send({ message: "Server Error" });
+    res.status(500).send({ message: "An error has occurred on the server" });
   }
 };
 
